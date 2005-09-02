6824e018e6ae4e44f46430adebe1ab4f3f56700b
krischik <krischik@3f7a923c-f673-49df-85a3-c442613b1839>
2005-09-02 18:19:33 +0000

    Yet another automated Wiki Download commit.

--- trunk/Document/local/config.php
+++ trunk/Document/local/config.php
@@ -64,6 +64,10 @@ $EnableGUIButtons = 1;
 $EnableDirectDownload = 0;
 $EnableUpload         = 1;
 $EnableTransitions    = 0;
+$UploadMaxSize = 1000000;
+$UploadExtSize['gif'] = 200000;
+$UploadExts['bash'] = 'application/x-shellscript';
+$UploadExts['cmd'] = 'application/x-cmdscript';
 
 ##  Setting $EnableDiag turns on the ?action=diag and ?action=phpinfo
 ##  actions, which often helps the PmWiki authors to troubleshoot 
