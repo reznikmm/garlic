6177480bdecc326446cc39d6c711ac83f1a0ce83
Maxim Reznik <reznikmm@gmail.com>
2020-02-08 11:59:56 +0200

    Set timeout for tests

--- Tests/run.sh
+++ Tests/run.sh
@@ -5,7 +5,11 @@ DIR=`dirname "$0"`
 export PATH=$1/bin:$PATH
 unset LANG
 
-$DIR/examples/bank.sh
-$DIR/examples/messages.sh
-$DIR/examples/racw.sh
-$DIR/examples/self.sh
+T=20
+
+timeout $T $DIR/examples/bank.sh
+timeout $T $DIR/examples/messages.sh
+timeout $T $DIR/examples/racw.sh
+timeout $T $DIR/examples/self.sh
+
+echo Tests completed
